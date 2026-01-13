a8b5bae5acd1d3e16e0a771c93dbb9ea6cdd9b6a
макс <bohacmaxym45@gmail.com>
2026-01-13 21:32:33 +0200

    2

--- src/main/java/com/maxim/tacionian/Tacionian.java
+++ src/main/java/com/maxim/tacionian/Tacionian.java
@@ -4,8 +4,10 @@ import com.maxim.tacionian.energy.PlayerEnergy;
 import com.maxim.tacionian.network.NetworkHandler;
 import com.maxim.tacionian.register.ModBlocks;
 import com.maxim.tacionian.register.ModItems;
+import com.maxim.tacionian.command.EnergyCommand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -16,17 +18,18 @@ public class Tacionian {
     public static final String MOD_ID = "tacionian";
 
     public Tacionian() {
-        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        var modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModItems.ITEMS.register(bus);
-        ModBlocks.BLOCKS.register(bus);
+        ModItems.ITEMS.register(modBus);
+        ModBlocks.BLOCKS.register(modBus);
 
-        bus.addListener(this::commonSetup);
+        modBus.addListener(this::commonSetup);
+        modBus.addListener(this::registerCaps); // Реєструємо капси правильно через лістенер
 
-        // ДОДАЙ ЦЕЙ РЯДОК: реєструємо цей клас на шині моду для роботи registerCaps
-        bus.register(this);
+        // РЕЄСТРАЦІЯ КОМАНД (на правильній шині!)
+        MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
 
-        // Реєстрація на загальній шині (для команд та інших подій)
+        // Решта ігрових подій (якщо є методи з @SubscribeEvent для Forge Bus)
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -34,13 +37,13 @@ public class Tacionian {
         event.enqueueWork(NetworkHandler::register);
     }
 
-    // Тепер цей метод точно спрацює, бо ми додали bus.register(this)
-    @SubscribeEvent
+    // Подія для команд (БЕЗ @SubscribeEvent, бо ми додали addListener в конструкторі)
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+        EnergyCommand.register(event.getDispatcher());
+    }
+
+    // Подія для Капабіліті (через Mod Bus)
     public void registerCaps(RegisterCapabilitiesEvent event) {
         event.register(PlayerEnergy.class);
-    }
-    @SubscribeEvent
-    public void onRegisterCommands(net.minecraftforge.event.RegisterCommandsEvent event) {
-        com.maxim.tacionian.command.EnergyCommand.register(event.getDispatcher());
     }
 }
