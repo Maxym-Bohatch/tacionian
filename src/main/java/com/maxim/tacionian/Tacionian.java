6c308eef6c67b5d06f64c833b1226c25e3865e80
макс <bohacmaxym45@gmail.com>
2026-01-13 21:36:34 +0200

    2

--- src/main/java/com/maxim/tacionian/Tacionian.java
+++ src/main/java/com/maxim/tacionian/Tacionian.java
@@ -8,7 +8,7 @@ import com.maxim.tacionian.command.EnergyCommand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,32 +18,33 @@ public class Tacionian {
     public static final String MOD_ID = "tacionian";
 
     public Tacionian() {
-        var modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        // Отримуємо шину моду (для реєстрації предметів, блоків, капсів)
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        // 1. Реєстрація контенту
         ModItems.ITEMS.register(modBus);
         ModBlocks.BLOCKS.register(modBus);
 
+        // 2. Слухачі шини МОДУ
         modBus.addListener(this::commonSetup);
-        modBus.addListener(this::registerCaps); // Реєструємо капси правильно через лістенер
+        modBus.addListener(this::registerCaps);
 
-        // РЕЄСТРАЦІЯ КОМАНД (на правильній шині!)
+        // 3. Слухачі загальної шини FORGE (Команди та інше)
+        // Використовуємо статичне посилання, щоб уникнути помилок з 'this'
         MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
-
-        // Решта ігрових подій (якщо є методи з @SubscribeEvent для Forge Bus)
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(NetworkHandler::register);
     }
 
-    // Подія для команд (БЕЗ @SubscribeEvent, бо ми додали addListener в конструкторі)
-    public void onRegisterCommands(RegisterCommandsEvent event) {
+    // Реєстрація команд
+    private void onRegisterCommands(RegisterCommandsEvent event) {
         EnergyCommand.register(event.getDispatcher());
     }
 
-    // Подія для Капабіліті (через Mod Bus)
-    public void registerCaps(RegisterCapabilitiesEvent event) {
+    // Реєстрація Capability
+    private void registerCaps(RegisterCapabilitiesEvent event) {
         event.register(PlayerEnergy.class);
     }
 }
