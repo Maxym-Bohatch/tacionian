c75cb2cf48bd3edb7b9326bb6e0df89879dcb301
макс <bohacmaxym45@gmail.com>
2026-01-13 21:39:04 +0200

    2

--- src/main/java/com/maxim/tacionian/client/ClientEvents.java
+++ src/main/java/com/maxim/tacionian/client/ClientEvents.java
@@ -1,20 +1,16 @@
 package com.maxim.tacionian.client;
 
 import com.maxim.tacionian.client.hud.EnergyHudOverlay;
-import com.maxim.tacionian.energy.PlayerEnergy;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = "tacionian", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientEvents {
-    @SubscribeEvent
-    public static void registerCaps(RegisterCapabilitiesEvent event) {
-        event.register(PlayerEnergy.class);
-    }
+
     @SubscribeEvent
     public static void registerOverlays(RegisterGuiOverlaysEvent event) {
+        // Реєструємо шар інтерфейсу поверх усіх інших (AboveAll)
         event.registerAboveAll("tacionian_energy", EnergyHudOverlay.HUD);
     }
 }
